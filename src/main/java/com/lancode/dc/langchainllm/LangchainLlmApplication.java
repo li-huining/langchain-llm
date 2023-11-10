ca55bb71667956439efcfa406fd277094434185d
lichun <lichun@lancode.com>
2023-11-10 18:04:30 +0800

    Initial commit

---
+++ src/main/java/com/lancode/dc/langchainllm/LangchainLlmApplication.java
@@ -0,0 +1,13 @@
+package com.lancode.dc.langchainllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangchainLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangchainLlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lancode/dc/langchainllm/LangchainLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lancode.dc.langchainllm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangchainLlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
