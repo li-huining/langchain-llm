9ed5b89bd178c0341a97c4fa9623742b1af60b56
lichun <lichun@lancode.com>
2023-11-10 20:50:03 +0800

    feat:增加默认端口号

--- src/main/java/com/lancode/dc/langchainllm/LangchainLlmApplication.java
+++ src/main/java/com/lancode/dc/langchainllm/LangchainLlmApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LangchainLlmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LangchainLlmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LangchainLlmApplication.class, args);
+    }
 
 }
